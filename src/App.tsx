a1e4b469fc1e03980533a363126874959aa572ab
lalli-oni <lalli.oni@gmail.com>
2025-01-03 15:24:33 +0100

    Add UserProvider to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 // App.tsx
 import React from 'react';
+
 import Header from './components/Header';
 import Welcome from './components/Welcome';
 import InterestingToDO from './components/InterestingToDo';
 import EventDetails from './components/EventDetails';
 import FAQ from './components/FAQ';
 import Footer from './components/Footer';
-import logo from './logo.svg';
+
 import './App.css';
 
+import UserProvider from './contexts/UserProvider';
+
+
 const App: React.FC = () => {
+
   return (
     <div className="App">
-      <Header />
-      <Welcome />
-      <EventDetails />
-      <FAQ />
-      <InterestingToDO />
-      <Footer />
+      <UserProvider>
+        <Header />
+        <Welcome />
+        <EventDetails />
+        <FAQ />
+        <InterestingToDO />
+        <Footer />
+      </UserProvider>
     </div>
   );
 };
