2a29ceb750520b11e5ff267c8e35e8a442023336
AleJuliet <garnetjuliet@gmail.com>
2025-01-04 13:32:49 +0100

    Update font styles and event details in the application

--- src/components/EventDetails.tsx
+++ src/components/EventDetails.tsx
@@ -4,10 +4,11 @@ import React from 'react';
 const EventDetails: React.FC = () => {
   return (
     <section id="event-details">
+      
       <h2>Event Details</h2>
-      <p>Date: [Your Wedding Date]</p>
-      <p>Time: [Your Wedding Time]</p>
-      <p>Location: [Wedding Venue]</p>
+      <p>Date: 26th of July 2025</p>
+      <p>Time: 3:00pm</p>
+      <p>Location: <a href="https://maps.app.goo.gl/NkC1pZp8j279ku7a8">Miðgarður, 301 - Akranes, Iceland</a></p>
     </section>
   );
 };
