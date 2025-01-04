633c3396ac6353909e9a901b8222c4e9d678bab5
AleJuliet <garnetjuliet@gmail.com>
2025-01-04 22:18:39 +0100

    Add Cabin component and integrate routing with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // App.tsx
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 
 import UserProvider from './contexts/UserProvider';
 
@@ -10,20 +11,37 @@ import EventDetails from './components/EventDetails';
 import FAQ from './components/FAQ';
 import Footer from './components/Footer';
 import Accommodation from './components/Accommodation';
+import Cabin from './components/Cabin';
 
 import './App.css';
 
 const App: React.FC = () => {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
+};
+
+const AppContent: React.FC = () => {
+  const location = useLocation();
 
   return (
     <div className="App">
       <UserProvider>
-        <Header />
-        <Welcome />
-        <EventDetails />
-        <Accommodation />
-        <FAQ />
-        <InterestingToDO />
+        {location.pathname === '/' && <Header />}
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Welcome />
+              <EventDetails />
+              <Accommodation />
+              <FAQ />
+              <InterestingToDO />
+            </>
+          } />
+          <Route path="/cabin" element={<Cabin />} />
+        </Routes>
         <Footer />
       </UserProvider>
     </div>
