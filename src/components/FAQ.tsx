800462fc6db18058d57ec690a2d78c07d4444478
AleJuliet <garnetjuliet@gmail.com>
2025-01-04 20:50:06 +0100

    Update FAQ section and modify background colors in the application

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -5,7 +5,18 @@ const FAQ: React.FC = () => {
   return (
     <section id="FAQ">
       <h2>FAQ</h2>
-      <p>bla bla.</p>
+      <div id="sectionFAQ" >
+      <h3>What is the agenda?</h3>
+      <p>We are working on it and will let you know! But for now we know that there will be a viking ceremony, snacks, drinks, dancing and very informal dinner with hot dogs</p>
+      <h3>What is the dress code?</h3>
+      <p>There is no dress code, but we would love to see you in your best festival outfit!</p>
+      <h3>Can I bring my kids?</h3>
+      <p>We know is difficult for some of you to leave your kids at home, so they are welcome too (they are not so bad). But we also want to have fun with you as much as we can, and not having you been a responsible adult/parent (we are selfish), so it's up to you!</p>
+      <h3>What about gifts?</h3>
+      <p>No gifts. We only ask you to make and record memories with us, and share it with us later!</p>
+      <h3>Who chose these ugly website colors?</h3>
+      <p>We are developers, not designers ok?</p>
+      </div>
     </section>
   );
 };
