0f7cc8b053efd7de27dd23f2dc9729acbe5fc562
lalli-oni <lalli.oni@gmail.com>
2025-01-06 13:59:22 +0100

    Fill in some of the accomodation content

--- src/components/Accommodation.tsx
+++ src/components/Accommodation.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 
 import { UserStateContext } from '../contexts/userContext';
 
@@ -17,10 +18,33 @@ const Accommodation: React.FC = () => {
       {userState !== null && userState.preferredLodging !== undefined ?
         <div style={{ paddingTop: "1rem" }}>
           {userState.preferredLodging === null ? <img src="/sad-drenched.gif" alt="lalli sad you wont be hanging out with us" /> : null}
-          {userState.preferredLodging === 'camping' ? <div>camping content</div> : null}
-          {userState.preferredLodging === 'moar-cabins' ? <div>moar cabins content</div> : null}
-          {userState.preferredLodging === 'moar-room' ? <div>moar room/guesthouse content</div> : null}
-          {userState.preferredLodging === 'other' ? <div>other lodging content</div> : null}
+          {userState.preferredLodging === 'camping' ?
+            <div>
+              <p>
+                Great! We will have camping areas right outside the venue and at Móar. We will try to ensure you access to toilets, internet and electricity.
+              </p>
+              <p>
+                WIP: here we will have suggestions to tents, live-in vans, caravans or other options for you to get the most out of your camping experience!
+              </p>
+            </div>
+          : null}
+          {userState.preferredLodging === 'moar-cabins' ?
+            <div>
+              <p>Móar has some nice simple cabins available for rent on a discount. WIP: add price. Please be aware, they are in limited supply so we have to prioritize.</p>
+              <p>For more information you can look <Link to="/cabin">here</Link> or visit Móar official site <a href="https://www.traveltomoar.com/" target="_blank" rel="noopener noreferrer">here</a>.</p>
+            </div>
+          : null}
+          {userState.preferredLodging === 'moar-room' ?
+            <div>
+              <p>Móar also has 4 rooms in their guesthouse. Since they are so few available we will have to prioritize.</p>
+              <p>For more information you can look <Link to="/guesthouse">here</Link> or visit Móar official site <a href="https://www.traveltomoar.com/" target="_blank" rel="noopener noreferrer">here</a>.</p>
+            </div>
+          : null}
+          {userState.preferredLodging === 'other' ?
+            <div>
+              <p>If you'd like to stay at a hotel, airbnb or anything else relatively close to the venue we wanted to make it easier by compiling a few options for you here.</p>
+            </div>
+          : null}
         </div>
       : null}
     </section>
