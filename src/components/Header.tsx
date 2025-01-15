ab333b61e99bf797feb965d0a45fbd7fde4aee95
lalli-oni <lalli.oni@gmail.com>
2025-01-15 11:00:08 +0100

    Remove manual js styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,7 +17,6 @@ const Header: React.FC = () => {
   }, [visibleSection]);
 
   useEffect(() => {
-    const header = document.getElementsByTagName('header')[0];
     const sections = document.querySelectorAll('section');
 
     const observer = new IntersectionObserver(
@@ -25,12 +24,6 @@ const Header: React.FC = () => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
             setVisibleSection(entry.target.id);
-
-            if (entry.target.id === 'welcome') {
-              header!.style.backgroundColor = 'transparent';
-            } else {
-              header!.style.backgroundColor = '#479249';
-            }
           }
         });
       },
