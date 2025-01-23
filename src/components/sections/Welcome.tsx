35971e7fe7b7d726dcd64b638a3266ec4694e1e2
lalli-oni <lalli.oni@gmail.com>
2025-01-23 14:05:53 +0100

    Less welcome text

--- src/components/sections/Welcome.tsx
+++ src/components/sections/Welcome.tsx
@@ -3,11 +3,10 @@ import React from 'react';
 const Welcome: React.FC = () => {
   return (
     <section id="welcome" style={{ padding: '8rem' }}>
-      <h1 style={{ color: 'orange' }}>Welcome to the wedding of <br/>Alechandrina & Lárus</h1>
+      <h1 style={{ color: 'orange' }}>The Wedding of<br/>Alechandrina & Lárus</h1>
       <p>
-        We're so excited to celebrate our love with all of you! 
-        We want to make it a festival where we will share laughters, music, games
-        and make some unforgettable memories with you.
+        We're so excited to celebrate our love with all of you!
+        Join us for this one time event only. An unforgettable wedding festival!
       </p>
     </section>
   );
