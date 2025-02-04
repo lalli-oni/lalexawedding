33cc7618d0894f813e14863a2e80971584564eb9
lalli-oni <lalli.oni@gmail.com>
2025-02-04 13:52:44 +0100

    Add new Travel page

--- src/components/sections/Travel.tsx
+++ src/components/sections/Travel.tsx
@@ -4,46 +4,8 @@ const Travel: React.FC = () => {
   return (
     <section id="travel">
       <h2>Traveling in Iceland</h2>
-      <p>
-        Iceland is a beautiful country, with unique landscapes and very interesting history. We highly recommend natural hot pools and the municipal swimming pool (practically every town has at least one!).
-      </p>
-      <h3>Car rentals</h3>
-      <p>Blue car rental has provided us with 10% discount if you use the following link: <a href="https://www.bluecarrental.is/?affiliate=Lalexa2025">https://www.bluecarrental.is/?affiliate=Lalexa2025</a>. Please make sure you see the discounted price when selecting the car!</p>
-      <h3>Close to the wedding venue</h3>
-      <p>
-        The wedding will take place in Akranes, a small town, but here are a few things you can do in the area: 
-        <a href="https://www.skagalif.is/is/visit-akranes">Visit Akranes</a> (translation available upper right corner).<br />
-        We highly recommend the hot pools and the municipal swimming pools! Hot pools are mini paradises in Iceland.
-      </p>
-      <ul>
-        <li><a href="https://www.facebook.com/Gudlaug.NaturalPool">Guðlaug natural pools</a></li>
-        <li><a href="https://www.facebook.com/Akranesviti/">Akranes light house</a></li>
-        <li><a href="http://www.hvammsvik.com/">Hvammsvík hot springs</a></li>
-      </ul>
-      <h3>"The city" - Reykjavík</h3>
-      <p>
-        Downtown is Reykjavík is relatively small but vibrant and full of surprises. Notable list of places to check out:
-      </p>
-      <ul>
-        <li><a href="https://www.phallus.is/">The penis museum</a></li>
-        <li><a href="https://www.skylagoon.com/">Sky Lagoon</a></li>
-      </ul>
-      <h3>Other places in Iceland</h3>
-      <p>
-        You can take the <a href="https://www.visiticeland.com/article/the-golden-circle">The Golden Circle</a> which is a half-day trip just outside of the city in the direction of the wedding venue.
-        There you'll find Alþingi, UNESCO world heritage site, world first parliament, where Europe and North America continental shelves meet, a place of execution and Game of Thrones shooting location.
-        Gullfoss, Europes largest waterfall. And Geysir (where the english word for geyser comes from!).
-      </p>
-      <p>
-        The south coast also has a lot of great attractions, great waterfalls, epic black sandy beaches and other epic shit!
-      </p>
-      <p>
-        On Reykjanes peninsula (where Keflavik airport is located) there are some interesting things happening, like a series of eruptions over the last few years. I'll try to ask my nationfolk to turn it on.
-        There is also the <a href="https://www.visiticeland.com/service-provider/5ec7d096a90548233654dd62">Blue Lagoon</a>, quite touristic but an amazing spectacle (the colors are really as strong as those internet images show!).
-      </p>
-      <p>
-        See here for other things to do in Iceland: <a href="https://www.visiticeland.com/">Visit Iceland</a> 
-      </p>
+      <p>We are excited for everyone who is visiting Iceland and we hope to make your visit as wonderful as possible. To that end, please reach out to us if you have any questions.</p>
+      <p>You can find more resources on visting Iceland <a href="/travel">here</a></p>
     </section>
   );
 };
