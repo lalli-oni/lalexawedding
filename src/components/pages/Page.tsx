b613272b6e74a9ea7dbe82ef583aa1a147c20119
lalli-oni <lalli.oni@gmail.com>
2025-02-05 16:27:54 +0100

    Add support to rotate hue on page bg

--- src/components/pages/Page.tsx
+++ src/components/pages/Page.tsx
@@ -3,17 +3,18 @@ import React, { ReactNode } from 'react';
 import './pages.css';
 
 interface PageProps {
+  rotateHue?: string; // Valid `hue-rotate` deg uom. example: '120'
   children: ReactNode;
 }
 
-const Page: React.FC<PageProps> = ({ children }) => {
+const Page: React.FC<PageProps> = ({ rotateHue, children }) => {
   return (
-    <>
-      <main className="page">
-        <a href="/" className="back-button">&lt; <span>Back</span></a>
+    <main className="page">
+      <a href="/" className="back-button">&lt; <span>Back</span></a>
+      <div style={{ backdropFilter: rotateHue ? `hue-rotate(${rotateHue}deg)` : undefined }}>
         {children}
-      </main>
-    </>
+      </div>
+    </main>
   );
 };
 
